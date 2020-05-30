6270543d50104e707a16fe22627c1b60517babf9
Bricx Carasco <bricxcarasco.gss@gmail.com>
2020-05-30 16:52:31 +0800

    Customize instagram-clone profile page

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -2,7 +2,29 @@ import React from 'react';
 
 const Profile = () => {
     return (
-        <h1>Profile</h1>
+        <div className="main-profile">
+            <div className="profile">
+                <div>
+                    <img className="profile-picture" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
+                </div>
+                <div>
+                    <h4>iambricxrain</h4>
+                    <div className="profile-count">
+                        <h6>40 posts</h6>
+                        <h6>40 followers</h6>
+                        <h6>40 following</h6>
+                    </div>
+                </div>
+            </div>
+            <div className="gallery">
+                <img className="gallery-image" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
+                <img className="gallery-image" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
+                <img className="gallery-image" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
+                <img className="gallery-image" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
+                <img className="gallery-image" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
+                <img className="gallery-image" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
+            </div>
+        </div>
     );
 }
  
