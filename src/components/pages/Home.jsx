30676fe5d93206a1de164cdd0d36def6662da736
Bricx Carasco <bricxcarasco.gss@gmail.com>
2020-05-30 17:08:02 +0800

    Customize home page component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,7 +2,46 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="home">
+            <div className="card home-card">
+                <h5>iambricxrain</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1470813740244-df37b8c1edcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80" alt=""/>
+                </div>
+                <div className="card-content">
+                    <i class="material-icons home-favorite">favorite</i>
+                    <h6>Title</h6>
+                    <p>This is a instagram clone post</p>
+                    <input type="text" placeholder="Add a comment..."/>
+                </div>
+            </div>
+
+            <div className="card home-card">
+                <h5>iambricxrain</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1470813740244-df37b8c1edcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80" alt=""/>
+                </div>
+                <div className="card-content">
+                    <i class="material-icons home-favorite">favorite</i>
+                    <h6>Title</h6>
+                    <p>This is a instagram clone post</p>
+                    <input type="text" placeholder="Add a comment..."/>
+                </div>
+            </div>
+
+            <div className="card home-card">
+                <h5>iambricxrain</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1470813740244-df37b8c1edcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80" alt=""/>
+                </div>
+                <div className="card-content">
+                    <i class="material-icons home-favorite">favorite</i>
+                    <h6>Title</h6>
+                    <p>This is a instagram clone post</p>
+                    <input type="text" placeholder="Add a comment..."/>
+                </div>
+            </div>
+        </div>
     );
 }
  
