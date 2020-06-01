58ef6ef55f6338feaae10e5a5d3082e6b1602282
Bricx Carasco <bricxcarasco.gss@gmail.com>
2020-06-01 16:23:24 +0800

    Integrate create post api and image upload in cloudinary for create post component

--- src/components/pages/CreatePost.jsx
+++ src/components/pages/CreatePost.jsx
@@ -1,22 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import M from 'materialize-css';
 
 const CreatePost = () => {
+    const history = useHistory();
+
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [image, setImage] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+
+    const sendImage = () => {
+        const imageForm = new FormData();
+        imageForm.append("file", image);
+        imageForm.append("upload_preset", "instagram-clone");
+        imageForm.append("cloud_name", "gss-bricx-carasco");
+        fetch(process.env.REACT_APP_IMAGE_BUCKET_API, {
+            method: "POST",
+            body: imageForm
+        })
+        .then(response => response.json())
+        .then(data => {
+            setImageUrl(data.secure_url);
+            sendPost();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    const sendPost = () => {
+        fetch('/createpost', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title,
+                body,
+                photo: imageUrl
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                M.toast({
+                    html: data.error,
+                    classes: "#e53935 red darken-1"
+                })
+            } else {
+                M.toast({
+                    html: "Successfully posted",
+                    classes: "#43a047 green darken-1"
+                })
+                history.push('/');
+            }
+        });
+    }
+
     return (
         <div className="card input-field create-post">
-            <input type="text" placeholder="title" />
+            <input 
+                type="text" 
+                placeholder="title" 
+                value={title}
+                onChange={event => setTitle(event.target.value)}
+                />
 
-            <input type="text" placeholder="body" />
+            <input 
+                type="text" 
+                placeholder="body" 
+                value={body}
+                onChange={event => setBody(event.target.value)}
+                />
 
             <div className="file-field input-field">
                 <div className="btn #64b5f6 blue darken-2">
                     <span>Upload Image</span>
-                    <input type="file" />
+                    <input 
+                        type="file" 
+                        onChange={event => setImage(event.target.files[0])}
+                        />
                 </div>
                 <div className="file-path-wrapper">
                     <input className="file-path validate" type="text" />
                 </div>
             </div>
-            <button className="btn waves-effect waves-light #64b5f6 blue darken-2">Submit Post</button>
+
+            <button 
+                className="btn waves-effect waves-light #64b5f6 blue darken-2"
+                onClick={() => sendImage()}
+                >Submit Post</button>
         </div>
     );
 }
