aafe4c126c3c55401d4fac4e99e860ab1daa1db2
Bricx Carasco <bricxcarasco.gss@gmail.com>
2020-06-06 14:52:37 +0800

    Integrate mypost api in profile component

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { UserContext } from '../../App';
 
 const Profile = () => {
+    const { state, dispatch } = useContext(UserContext);
+    const [myPostImage, setMyPostImage] = useState([]);
+
+    useEffect(() => {
+        fetch('/mypost', {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("jwt")}`
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            setMyPostImage(data.myposts);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }, []);
+
     return (
         <div className="main-profile">
             <div className="profile">
@@ -8,7 +27,7 @@ const Profile = () => {
                     <img className="profile-picture" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
                 </div>
                 <div>
-                    <h4>iambricxrain</h4>
+                    <h4>{state ? state.name : 'loading...'}</h4>
                     <div className="profile-count">
                         <h6>40 posts</h6>
                         <h6>40 followers</h6>
@@ -17,12 +36,13 @@ const Profile = () => {
                 </div>
             </div>
             <div className="gallery">
-                <img className="gallery-image" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
-                <img className="gallery-image" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
-                <img className="gallery-image" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
-                <img className="gallery-image" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
-                <img className="gallery-image" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
-                <img className="gallery-image" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
+                {
+                    myPostImage.map(mypost => {
+                        return (
+                            <img key={mypost._id} className="gallery-image" src={mypost.photo} alt={mypost.title}/>
+                        );
+                    })
+                }
             </div>
         </div>
     );
