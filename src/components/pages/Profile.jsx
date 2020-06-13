80f36ad3fd2ea48f0f7adce77f77a8094fe672e5
Bricx Carasco <bricxcarasco.gss@gmail.com>
2020-06-13 16:54:11 +0800

    Integrate follow/unfollow api and logic functions in user profile component

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { UserContext } from '../../App';
 const Profile = () => {
     const { state } = useContext(UserContext);
     const [myPostImage, setMyPostImage] = useState([]);
+    console.log(state);
 
     useEffect(() => {
         fetch('/my-post', {
@@ -21,30 +22,36 @@ const Profile = () => {
     }, []);
 
     return (
-        <div className="main-profile">
-            <div className="profile">
-                <div>
-                    <img className="profile-picture" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
-                </div>
-                <div>
-                    <h4>{state ? state.name : 'loading...'}</h4>
-                    <div className="profile-count">
-                        <h6>40 posts</h6>
-                        <h6>40 followers</h6>
-                        <h6>40 following</h6>
+        <React.Fragment>
+            {
+                state ? 
+                <div className="main-profile">
+                    <div className="profile">
+                        <div>
+                            <img className="profile-picture" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
+                        </div>
+                        <div>
+                            <h4>{ state.name }</h4>
+                            <div className="profile-count">
+                                <h6>{ myPostImage.length } posts</h6>
+                                <h6>{ state.followers.length } followers</h6>
+                                <h6>{ state.following.length } following</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="gallery">
+                        {
+                            myPostImage.map(mypost => {
+                                return (
+                                    <img key={mypost._id} className="gallery-image" src={mypost.photo} alt={mypost.title}/>
+                                );
+                            })
+                        }
                     </div>
                 </div>
-            </div>
-            <div className="gallery">
-                {
-                    myPostImage.map(mypost => {
-                        return (
-                            <img key={mypost._id} className="gallery-image" src={mypost.photo} alt={mypost.title}/>
-                        );
-                    })
-                }
-            </div>
-        </div>
+                : <h2 className="user-profile-loading center-align">loading...</h2>
+            }
+        </React.Fragment>
     );
 }
  
--- src/components/pages/UserProfile.jsx
+++ src/components/pages/UserProfile.jsx
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { UserContext } from '../../App';
+
 
 const UserProfile = () => {
+    const { id } = useParams();
+    const { state, dispatch } = useContext(UserContext);
+
     const [ userPost, setUserPost ] = useState([]);
     const [ user, setUser ] = useState({});
-    const { id } = useParams();
+    const [ isFollow, setIsFollow ] = useState(false);
 
     useEffect(() => {
         fetch(`/user/${id}`, {
@@ -16,11 +21,84 @@ const UserProfile = () => {
         .then(profile => {
             setUser(profile.user);
             setUserPost(profile.posts);
+            if (profile.user.followers.includes(state._id)) {
+                setIsFollow(true);
+            }
         })
         .catch(error => {
             console.log(error);
         });
     },[id]);
+
+    const followUser = () => {
+        fetch('/follow', {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("jwt")}`
+            },
+            body: JSON.stringify({
+                id
+            })
+        })
+        .then(response => response.json())
+        .then(result => {
+            dispatch({
+                type: "UPDATE",
+                payload: {
+                    followers: result.followers,
+                    following: result.following
+                }
+            });
+            localStorage.setItem("user", JSON.stringify(result));
+            setUser((prevState) => {
+                return {
+                    ...prevState,
+                    followers:  [...prevState.followers, result._id]
+                }
+            });
+            setIsFollow(true);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
+    const unfollowUser = () => {
+        fetch('/unfollow', {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("jwt")}`
+            },
+            body: JSON.stringify({
+                id
+            })
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            dispatch({
+                type: "UPDATE",
+                payload: {
+                    followers: result.followers,
+                    following: result.following
+                }
+            });
+            localStorage.setItem("user", JSON.stringify(result));
+            setUser((prevState) => {
+                const setFollowers = prevState.followers.filter(follower => follower !== result._id);
+                return {
+                    ...prevState,
+                    followers: setFollowers
+                }
+            });
+            setIsFollow(false);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
 
     return (
         <React.Fragment>
@@ -32,12 +110,27 @@ const UserProfile = () => {
                             <img className="profile-picture" src="https://images.unsplash.com/photo-1551179939-b839002d0a18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" alt=""/>
                         </div>
                         <div>
-                            <h4>{ user.name }</h4>
+                            <h4>
+                                { user.name }
+
+                                {
+                                    isFollow ? 
+                                    <button 
+                                        className="follow-button btn-small right #e53935 red darken-1"
+                                        onClick={unfollowUser}
+                                        >Unfollow</button>
+                                    :
+                                    <button 
+                                        className="follow-button btn-small right #0094f6 blue darken-1"
+                                        onClick={followUser}
+                                        >Follow</button>
+                                }
+                            </h4>
                             <h5>{ user.email }</h5>
                             <div className="profile-count">
                                 <h6>{ userPost.length } posts</h6>
-                                <h6>40 followers</h6>
-                                <h6>40 following</h6>
+                                <h6>{ user.followers.length } followers</h6>
+                                <h6>{ user.following.length } following</h6>
                             </div>
                         </div>
                     </div>
