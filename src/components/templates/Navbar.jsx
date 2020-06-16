ff968934b7873debea3e5634944e067b51c99e03
Bricx Carasco <bricxcarasco.gss@gmail.com>
2020-06-16 15:34:46 +0800

    Make navbar mobile responsive

--- src/components/templates/Navbar.jsx
+++ src/components/templates/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { UserContext } from '../../App';
+import M from 'materialize-css';
 
-const Navbar = () => {
+const NavbarT = () => {
     const history = useHistory();
 
     const { state, dispatch } = useContext(UserContext);
@@ -35,16 +36,59 @@ const Navbar = () => {
         }
     }
 
+    const renderListMobile = () => {
+        if (state) {
+            return [
+                <li key="0"><a href="/following">Posts</a></li>,
+                <li key="1" ><a href="/profile">Profile</a></li>,
+                <li key="2"><a href="/create">Create Post</a></li>,
+                <li key="3">
+                    <button 
+                    className="btn #c62828 red darken-3 btn-logout"
+                    onClick={() => logoutFunction()}>Logout</button>
+                </li>
+            ];
+        } else {
+            return [
+                <li key="4"><Link to="/signin">Signin</Link></li>,
+                <li key="5"><Link to="/signup">Signup</Link></li>
+            ];
+        }
+    }
+
     return (
-        <nav>
-            <div className="nav-wrapper white">
-            <Link to={state ? "/" : "/signin"} className="brand-logo left">Instagram</Link>
-            <ul id="nav-mobile" className="right">
-                { renderList() }
+        <>
+            <nav>
+                <div className="nav-wrapper white">
+                    <div className="container">
+                        <Link to={state ? "/" : "/signin"} className="brand-logo">Instagram</Link>
+                        <a 
+                            href="/" 
+                            data-target="mobile-demo" 
+                            className="sidenav-trigger">
+                            <i className="material-icons">menu</i>
+                        </a>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
+                            { renderList() }
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <ul className="sidenav" id="mobile-demo">
+                {
+                    renderListMobile()
+                }
             </ul>
-            </div>
-        </nav>
+
+            {
+                document.addEventListener('DOMContentLoaded', () => {
+                    let elems = document.querySelectorAll('.sidenav');
+                    M.Sidenav.init(elems);
+                })
+            }
+        </>
     );
 }
  
-export default Navbar;
+export default NavbarT;
